760958224e0d7957fb44fd2f730df7c760690936
Mikhail Papkov <mikhail.papkov@gmail.com>
2016-07-28 00:10:49 +0300

    scoring file

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from sklearn.ensemble import RandomForestClassifier
 from numpy import genfromtxt, savetxt
  
 
-
 path = 'compressed/75_36_8_8_3531_compressed.png'
 image = Image.open(path).convert('L')
 image = np.array(image)
@@ -19,14 +18,14 @@ print(image.shape)
 
 image_line = np.reshape(image, image.shape[0]*image.shape[1])
 print(image_line.shape)
-#create the training & test sets, skipping the header row with [1:]
+# create the training & test sets, skipping the header row with [1:]
 dataset = genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
 target = [x[0] for x in dataset]
 train = [x[1:] for x in dataset]
 test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
 
-#create and train the random forest
-#multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+# create and train the random forest
+# multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
 rf = RandomForestClassifier(n_estimators=100)
 rf.fit(train, target)
 
